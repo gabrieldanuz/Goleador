f6a82879bffdadb4d20059c40e0c9e706c7cb9f9
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2022-06-03 23:24:10 -0300

    Dando seguimento ao aplicativo

--- src/Goleador/index.js
+++ src/Goleador/index.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
+
+import {useNavigation} from '@react-navigation/native'
 
 export default function Goleador() {
+  const navigation = useNavigation();
+
  return (
    <View style={styles.container}>
-     <Text>Fala tu, Craque</Text>
+     <Text>Aqui você pode registrar o que você fez na partida</Text>
      <View>
         <TextInput 
         style={styles.quantGols}
         keyboardType="numeric"
-        placeholder='Marcou quantos gols hoje ?'
+        placeholder='Quantidade de gols ?'
         />
-                <TextInput 
+        <TextInput 
         style={styles.quantGols}
         keyboardType="numeric"
-        placeholder='Pifou quantas ?'
+        placeholder='Assistências ?'
         />
+        <TextInput 
+        style={styles.quantGols}
+        keyboardType="numeric"
+        placeholder='Bolas na trave ?'
+        />
+
+        <TouchableOpacity style={styles.exit}
+        onPress={() => navigation.navigate('MenuPrincipal')}
+        >
+          <Text style={styles.textExit}>SAIR</Text>
+        </TouchableOpacity>
       </View>
    </View>
  );
@@ -32,7 +47,22 @@ quantGols: {
   justifyContent: 'center',
   alignItems: 'center',
   marginTop: 10,
-  backgroundColor: '#38A69D',
+  backgroundColor: '#36A69D',
   borderRadius: 8,
+  color: '#FFF',
+},
+exit: {
+  backgroundColor: '#FF0000',
+  width: '100%',
+  borderRadius: 4,
+  paddingVertical: 8,
+  marginTop: 14,
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+textExit: {
+  color: '#FFF',
+  fontSize: 20,
+  fontWeight: 'bold',
 }
 })
--- src/MenuPrincipal/index.js
+++ src/MenuPrincipal/index.js
@@ -1,10 +1,53 @@
 import React from 'react';
-import {View, Text, TextInput, StyleSheet} from 'react-native'
+import {View, Text, TextInput, StyleSheet, TouchableOpacity} from 'react-native';
+
+import {useNavigation} from '@react-navigation/native'
 
 export default function MenuPrincipal() {
+  const navigation = useNavigation();
  return (
-   <View>
-     <Text>Seja Bem Vindo, Goleador</Text>
+   <View style={styles.container}>
+     <Text style={styles.title}>Seja Bem Vindo, Goleador</Text>
+      <Text style={styles.description}>Aqui você pode registrar todos os gols que o Pelé não conseguiu</Text>
+
+      <View>
+        <TouchableOpacity style={styles.buttonPartida}
+        onPress={() => navigation.navigate('Goleador')}
+        >
+          <Text style={styles.textButton}>Registrar partida</Text>
+        </TouchableOpacity>
+      </View>
    </View>
  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F3F3F3',
+    padding: 18
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 28,
+    fontWeight: 'bold'
+  },
+  description: {
+    fontSize: 12,
+  },
+  buttonPartida: {
+    backgroundColor: '#36A69D',
+    width: '100%',
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 14,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textButton: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold'
+
+  }
+})
